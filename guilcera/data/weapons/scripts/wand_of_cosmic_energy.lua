0f5fe2773abece268360883a7ef2ec6fec3ab005
peonso <peonso@hotmail.com>
2014-06-02 13:23:40 -0300

    still 4.02

--- guilcera/data/weapons/scripts/wand_of_cosmic_energy.lua
+++ guilcera/data/weapons/scripts/wand_of_cosmic_energy.lua
@@ -3,6 +3,7 @@ setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_ENERGYDAMAGE)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0, -37, 0, -53)
 
+
 function onUseWeapon(cid, var)
 	return doCombat(cid, combat, var)
 end
