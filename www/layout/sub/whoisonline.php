82b11eb8e41dc4ae92db3cdb9a8a6f034285f7a5
peonso <peonso@hotmail.com>
2014-08-25 22:26:36 -0300

    small fixes
    hota enchanting script changed
    small fixes at items.xml
    [AAC] fixed bug at supports.php
    [AAC] fixed bug at whoisonline.php

--- www/layout/sub/whoisonline.php
+++ www/layout/sub/whoisonline.php
@@ -1,12 +1,11 @@
 <?php require_once 'engine/init.php';
 
 $records = mysql_select_multi('SELECT * FROM `server_record` ORDER BY `record` DESC LIMIT 1;');
-foreach ($records as $record) $data[] = $record;
 ?>
 <img src="layout/images/titles/t_plaonline.png"/><p>
 <table>
 <tr class="yellow"><th>Server Status</th></tr>
-<tr><td><b><?php echo $config['site_title'] ?></b> record of players online: <?php echo $data[0]['record'] ?> on <?php echo date("j M Y", $data[0]['timestamp']) ?>.</td></tr>
+<tr><td><b><?php echo $config['site_title'] ?></b> record of players online: <?php echo $records[0]['record'] ?> on <?php echo date("j M Y", $records[0]['timestamp']) ?>.</td></tr>
 <tr><td><?php
 								$status = true;
 								if ($config['status']['status_check']) {
